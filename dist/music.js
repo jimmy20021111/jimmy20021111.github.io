48cd091c6edf44307261f6bb7fcda3310fc64474
jimmy <a0909038608@gmail.com>
2024-06-10 16:21:44 +0800

    Site updated: 2024-06-10 16:21:43

---
+++ dist/music.js
@@ -0,0 +1,3 @@
+// build time:Mon Jun 10 2024 16:21:41 GMT+0800 (台北標準時間)
+const e=new APlayer({container:document.getElementById("aplayer"),fixed:false,autoplay:false,theme:"#b7daff",loop:"all",order:"list",preload:"auto",volume:.7,mutex:true,listFolded:false,listMaxHeight:"200px",lrcType:3,audio:[{name:"兩隻老虎",artist:"兒歌",url:"https://XXX.mp3",cover:"/music/cover/兩隻老虎.jpg",lrc:"/music/lyrics/兩隻老虎.lrc"},{name:"哥哥爸爸真偉大",artist:"兒歌",url:"https://XXX.mp3",cover:"/music/cover/哥哥爸爸真偉大.jpg",lrc:"/music/lyrics/哥哥爸爸真偉大.lrc"}]});
+//rebuild by hrmmi 
