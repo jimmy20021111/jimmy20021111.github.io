cab4d01cf8060dfb3af838bd5a5c0c1df15d8a2c
jimmy <a0909038608@gmail.com>
2024-06-13 00:52:32 +0800

    Site updated: 2024-06-13 00:52:32

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,3 @@
-// build time:Wed Jun 12 2024 21:21:08 GMT+0800 (台北標準時間)
+// build time:Thu Jun 13 2024 00:52:29 GMT+0800 (台北標準時間)
 const e=new APlayer({container:document.getElementById("aplayer"),fixed:false,autoplay:false,theme:"#b7daff",loop:"all",order:"list",preload:"auto",volume:.7,mutex:true,listFolded:false,listMaxHeight:"200px",lrcType:3,audio:[{name:"兩隻老虎",artist:"兒歌",url:"https://XXX.mp3",cover:"/music/cover/兩隻老虎.jpg",lrc:"/music/lyrics/兩隻老虎.lrc"},{name:"哥哥爸爸真偉大",artist:"兒歌",url:"https://XXX.mp3",cover:"/music/cover/哥哥爸爸真偉大.jpg",lrc:"/music/lyrics/哥哥爸爸真偉大.lrc"}]});
 //rebuild by hrmmi 
